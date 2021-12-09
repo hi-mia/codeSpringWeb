af54da7e60b686090c6696bab9021d00a662e302
idleline7 <idleline96@gmail.com>
2021-12-09 16:54:02 +0900

    챕2 MVC구조 진행중

---
+++ jex01/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,22 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() { //스프링 MVC의 기본 경로
+		return new String[] {"/"};
+	}
+
+}
